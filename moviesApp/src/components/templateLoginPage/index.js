6a335a8f170a5f633ebd93e11cd2c2971ff2d054
Máté Domonics <domonicsmate@gmail.com>
2022-12-31 00:28:03 +0000

    Implementing Material UI for login page

--- moviesApp/src/components/templateLoginPage/index.js
+++ moviesApp/src/components/templateLoginPage/index.js
@@ -1,12 +1,15 @@
 import React, { useContext, useState } from "react";  // useState/useEffect redundant 
 import { Navigate, Link } from 'react-router-dom';
 import { AuthContext } from "../../contexts/authContext"
-
+import Button from "@mui/material/Button"
+import Box from "@mui/material/Box"
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
 
 const TemplateLoginPage = props => {
     const context = useContext(AuthContext)
-    const [ username, setUsername ] = useState("");
-    const [ password, setPassword ] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
     const login = () => {
         context.authenticate(username, password);
@@ -18,10 +21,37 @@ const TemplateLoginPage = props => {
 
     return (
         <>
-            <h2>LoginPage</h2>
-            <input id="username" placeholder="username" onChange={e => { setUsername(e.target.value); }}></input>
-            <input id="password" placeholder="password" type = "password" onChange={e => { setPassword(e.target.value); }}></input>
-            <button onClick = {login}>login please I beg you</button>
+            <Box component="div" pt={2} sx={{ justifyContent: "center", display: "flex" }}>
+                <Typography variant= "h4">
+                    Login Page for TMDB
+                </Typography>
+            </Box>
+            <Box component="div" pt={2} sx={{ justifyContent: "center", display: "flex" }}>
+                <TextField
+                    id="username"
+                    label="Username"
+                    type="text"
+                    onChange={e => { setUsername(e.target.value); }}
+                >
+                </TextField>
+            </Box>
+            <Box component="div" pt={1} sx={{ justifyContent: "center", display: "flex" }}>
+                <TextField
+                    id="password"
+                    label="Password"
+                    type="password"
+                    onChange={e => { setPassword(e.target.value); }}
+                >
+                </TextField>
+            </Box>
+            <Box component="div" pt={2} sx={{ justifyContent: "center", display: "flex" }}>
+                <Button
+                variant = "contained"
+                color = "info"
+                onClick={login}>
+                    Login
+                </Button>
+            </Box>
         </>
     );
 };
